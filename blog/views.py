17f053a3680d9bd4054c7e6bd421e696589c34f4
Zamarin-Alexey <91373169+Zamarin-Alexey@users.noreply.github.com>
2022-11-02 19:01:52 +0300

    Добавлена форма поста

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'blog/home_page.html', {'title': 'Блог', 'description': 'Страница блога'})
+def home_page(request):
+    return render(request, 'blog/home_page.html', {'title': 'Блог'})
+
+
+def post(request, post_id):
+    return render(request, 'blog/post.html', {'title': 'Блог', 'post_id': post_id})
