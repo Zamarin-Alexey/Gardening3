bd3662fda00d80d6d45d26b118de7a815d13a2fb
Siavilia <siavilia@yandex.ru>
2022-11-02 14:14:27 +0300

    Работа с моделями

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField
+    estimation = models.PositiveSmallIntegerField()
