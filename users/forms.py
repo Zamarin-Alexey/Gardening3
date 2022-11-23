6282d4ec7c072c9c88b6cfe48a38fb5c831f83d3
Siavilia <siavilia@yandex.ru>
2022-11-23 21:26:13 +0300

    profile (html)

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Profile
+
+
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
                                required=True,
                                widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(required=True,
-                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
         fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.ClearableFileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
--- users/models.py
+++ users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default_avatar.jpg',
+                               upload_to='profile_images',
+                               verbose_name='Фотография')
+    bio = models.TextField(blank=True, verbose_name='Статус')
 
     def _str_(self):
         return self.user.username
-
-
-
-
-
