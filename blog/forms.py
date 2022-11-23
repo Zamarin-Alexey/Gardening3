7e659400265a8f3f7576c39856eefeaf6fe7c699
Zamarin-Alexey <91373169+Zamarin-Alexey@users.noreply.github.com>
2022-11-23 22:12:47 +0300

    Недоделанная карусель

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,4 @@ class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255, label='Заголовок')
     body = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Текст')
     tags = forms.CharField(required=False, max_length=255, label='Теги')
-    images = forms.ImageField(label=u'Фотографии', widget=forms.FileInput(attrs={'multiple': 'multiple'}))
+    images = forms.ImageField(required=False, label=u'Фотографии', widget=forms.FileInput(attrs={'multiple': 'multiple'}))
