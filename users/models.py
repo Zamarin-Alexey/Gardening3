66ee845268d5f7506c089a6cac6b7310d1fbd226
Siavilia <siavilia@yandex.ru>
2022-12-14 20:26:20 +0300

    add manyTOmany fields in Users

--- users/models.py
+++ users/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from plants.models import Plant
+from blog.models import Post
+
+
 def user_directory_path(instance, filename):
     return 'profile_images/user_{0}/{1}'.format(instance.user.id, filename)
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default='default_avatar.png',
                                upload_to=user_directory_path,
                                verbose_name='Фотография')
     bio = models.TextField(blank=True, verbose_name='Статус')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plants = models.ManyToManyField(Plant)
+    posts = models.ManyToManyField(Post)
 
     def _str_(self):
         return self.user.username
