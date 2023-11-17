d2408435839a5b40be8e63f6238c667593775455
Zamarin-Alexey <91373169+Zamarin-Alexey@users.noreply.github.com>
2023-11-17 21:08:07 +0300

    БД заменена на Postgres

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
     body = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Текст')
     tags = forms.CharField(required=False, max_length=255, label='Теги')
     images = forms.ImageField(required=False, label=u'Фотографии',
-                              widget=forms.FileInput(attrs={'multiple': 'multiple'}))
+                              widget=forms.FileInput())
     class Meta:
         model = Post
         fields = ['title', 'body', 'tags', 'images']
