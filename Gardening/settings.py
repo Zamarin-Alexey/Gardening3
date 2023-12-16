d4eb86d96faefee56884907c2dae4512b2198d11
Liliya Antonevich <siavilia@yandex.ru>
2023-12-16 17:18:28 +0300

    test

--- Gardening/settings.py
+++ Gardening/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os.path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,7 +31,7 @@ ALLOWED_HOSTS = ['158.160.104.118', 'localhost', '127.0.0.1', '0.0.0.0', ]
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    #'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
